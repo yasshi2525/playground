8f708e1454094e8d3f7b7e16b79b2c7d259504d7
yasshi2525 <yasshy2@gmail.com>
2025-02-11 14:25:55 +0900

    回転させながら落とす実装

---
+++ public/presets/confetti/6/script/main.js
@@ -0,0 +1,17 @@
+function main () {
+  const scene = new g.Scene({ game: g.game });
+  scene.onLoad.add(() => {
+    const paper = new g.FilledRect({ scene, x: g.game.width / 2, y: -100, width: 25, height: 25, anchorX: 0.5, anchorY: 0.5, cssColor: "red" });
+    paper.onUpdate.add(() => {
+      paper.y += 1;
+      paper.modified();
+      if (paper.y > g.game.height + 50) {
+        paper.destroy();
+      }
+    });
+    scene.append(paper);
+  })
+  g.game.pushScene(scene);
+}
+
+module.exports = main;
